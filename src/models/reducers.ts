7eb79c27e17455f6bc7a2d29d19891748195b631
李谷 <ligu@rlair.net>
2019-11-29 17:00:56 +0800

    默认配置信息加入store

--- src/models/reducers.ts
+++ src/models/reducers.ts
@@ -1,6 +1,6 @@
 import {combineReducers} from 'redux';
 import settingsReducer from './settings/settingsReducer';
 
-const reducers = combineReducers({settingsReducer});
+const reducers = combineReducers({settings: settingsReducer});
 
 export default reducers;
--- src/models/settings/settingsReducer.ts
+++ src/models/settings/settingsReducer.ts
@@ -1,6 +1,6 @@
 import defaultSettings from '@/config/defaultSettings';
 
-const settingsReducer = (state = {settings: defaultSettings}, action: {type: string}) => {
+const settingsReducer = (state = defaultSettings, action: {type: string}) => {
     switch (action.type) {
         default: return state;
     }
